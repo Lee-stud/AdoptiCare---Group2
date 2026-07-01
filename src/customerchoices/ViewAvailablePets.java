65d4f252591eef8788dfec1d006b332cb14bfe00
Lee-stud <lunalee694@gmail.com>
2026-07-01 23:48:38 +0800

    UPDATE 7/1/26 11:47PM
    UPDATES CUSTOMER MENU

--- src/customerchoices/ViewAvailablePets.java
+++ src/customerchoices/ViewAvailablePets.java
@@ -1,12 +1,51 @@
 package customerchoices;
 
+import database.DbConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ViewAvailablePets {
 
     public static void viewAvailablePets() {
         
         try {
             
+            System.out.println("\n===== AVAILABLE PETS =====");
             
+            Connection con = DbConnection.getConnection();
+            
+            String sql = "SELECT * "
+                    + "FROM pets "
+                    + "WHERE adoption_status = 'Available'";
+            
+            Statement st = con.createStatement();
+            
+            ResultSet rs = st.executeQuery(sql);
+            
+            if(rs.next()) {
+                
+                do {
+                    
+                    System.out.println("Pet ID: " + rs.getInt("pet_id"));
+                    
+                    System.out.println("Pet Name: " + rs.getString("pet_name"));
+                    
+                    System.out.println("Gender: " + rs.getString("gender"));
+                    
+                    System.out.println("Age: " + rs.getInt("age"));
+                    
+                    System.out.println("Species: " + rs.getString("species"));
+                    
+                    System.out.println("Breed:" + rs.getString("breed"));
+                    
+                    System.out.println("Description: " + rs.getString("description"));
+                    
+                    System.out.println("Adoption_Status: " + rs.getString("adoption_status"));
+                    
+                } while (rs.next());
+                
+            }
             
         } catch (Exception e) {
             System.out.println(e);
--- src/customerchoices/ViewPetDetails.java
+++ src/customerchoices/ViewPetDetails.java
@@ -10,40 +10,63 @@ import java.util.Scanner;
 public class ViewPetDetails {
 
     public static void viewPetDetails() {
-     
+
         Scanner input = new Scanner(System.in);
-        
-        try{
+
+        try {
             
+            System.out.println("\n===== PET DETAILS =====");
+
             Connection con = DbConnection.getConnection();
-            
-            String sql = "SELECT * FROM pets WHERE archived = 0";
-            
+
+            String sql
+                    = "SELECT p.*, pm.vaccine_name, pm.health_condition, "
+                    + "pm.last_vaccination_date, pm.next_vaccination_schedule, "
+                    + "pm.vaccination_status "
+                    + "FROM pets p "
+                    + "LEFT JOIN pet_medical_records pm "
+                    + "ON p.pet_id = pm.pet_id "
+                    + "WHERE p.archived = 0";
+
             Statement st = con.createStatement();
-            
+
             ResultSet rs = st.executeQuery(sql);
-            
+
             while (rs.next()) {
                 
                 System.out.println("\nPet ID: " + rs.getInt("pet_id"));
-                
+
                 System.out.println("Pet Name: " + rs.getString("pet_name"));
-                
+
                 System.out.println("Species: " + rs.getString("species"));
-                
+
                 System.out.println("Age: " + rs.getInt("age"));
-                
+
                 System.out.println("Gender: " + rs.getString("gender"));
-                
+
                 System.out.println("Breed: " + rs.getString("breed"));
+
+                System.out.println("Health Condition: " + rs.getString("health_condition"));
+
+                System.out.println("Vaccination Status: " + rs.getString("vaccination_status"));
+
+                System.out.println("Description: " + rs.getString("description"));
+                
+                System.out.println("\n===== " + rs.getString("pet_name") + "MEDICAL RECORD =====");
+                
+                System.out.println("Vaccine Name: " + rs.getString("vaccine_name"));
                 
                 System.out.println("Health Condition: " + rs.getString("health_condition"));
                 
+                System.out.println("Last Vaccination Date: " + rs.getTimestamp("last_vaccination_schedule"));
+                
+                System.out.println("Next Vaccination Schedule: " + rs.getTimestamp("next_vaccination_schedule"));
+                
                 System.out.println("Vaccination Status: " + rs.getString("vaccination_status"));
                 
-                System.out.println("Description: " + rs.getString("description"));
+                System.out.println("==============================");
             }
-            
+
         } catch (SQLException e) {
             System.out.println(e);
         }
